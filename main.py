785b04b2104f560d49e7e9b27c1b32e8addf65d0
ArturoGomez <arturogomezgz04@gmail.com>
2023-10-17 19:24:10 -0600

    error version 2x2

--- main.py
+++ main.py
@@ -6,22 +6,17 @@ pygame.init()
 pygame.font.init()
 pygame.mixer.init()
 
-# colores en RGB
-BLANCO = (255, 255, 255)
-NEGRO = (36, 36, 36)
-ROJO = (194, 59, 33)
-VERDE = (0, 107, 61)
-AZUL = (0, 0, 131)
-AMARILLO = (253, 204, 12)
-MORADO = (202, 62, 230)
-NARANJA = (255, 172, 4)
-ROSITA = (255, 124, 234)
-OSCAR_GAY = (79, 255, 51)
-
-
 # DIMENSIONES DE LA VENTANA
 WIDTH = 600
 HEIGHT = 650
+
+# colores en RGB
+WHITE = (255, 255, 255)
+BLACK = (36, 36, 36)
+RED = (194, 59, 33)
+GREEN = (0, 107, 61)
+BLUE = (0, 0, 131)
+YELLOW = (253, 204, 12)
 
 # Font
 MAIN_FONT = pygame.font.SysFont('Arial', 40)
@@ -51,16 +46,11 @@ def main():
     game_sequence = generate_color_sequence(sequence_length)
     player_sequence = []
 
-    color_rectangles = {  # color del rectangulo
-        ROJO: pygame.Rect(0, 100, 225, 225),  # x, y, largo y ancho del rectangulo
-        VERDE: pygame.Rect(0, 325, 225, 225),
-        AZUL: pygame.Rect(225, 100, 225, 225),
-        AMARILLO: pygame.Rect(225, 325, 225, 225),
-        NEGRO: pygame.Rect(0, 0, 0, 0),
-        MORADO: pygame.Rect(202, 62, 230, 255),
-        NARANJA: pygame.Rect(255, 172, 4, 255),
-        ROSITA: pygame.Rect(255, 124, 234, 255),
-        OSCAR_GAY: pygame.Rect(79, 255, 51, 255)
+    color_rectangles = {  # color : rectangle
+        RED: pygame.Rect(0, 100, 225, 225),  # x, y, largo y ancho del rectangulo
+        GREEN: pygame.Rect(0, 325, 225, 225),
+        BLUE: pygame.Rect(225, 100, 225, 225),
+        YELLOW: pygame.Rect(225, 325, 225, 225)
     }
 
     display_sequence(game_sequence)
@@ -106,23 +96,23 @@ def main():
 
 
 def generate_color_sequence(length):
-    return [random.choice([ROJO, VERDE, AZUL, AMARILLO, NEGRO, MORADO, NARANJA, ROSITA, OSCAR_GAY]) for _ in range(length)]
+    return [random.choice([RED, GREEN, BLUE, YELLOW]) for _ in range(length)]
 
 
-# regresa un string equivalente al color
+# Returns the string equivalent of the RGB color
 def get_color_string(color):
-    color_dictionary = {ROJO: "ROJO", AMARILLO: "AMARILLO", VERDE: "VERDE", AZUL: "AZUL", NEGRO: "NEGRO", MORADO: "MORADO", NARANJA: "NARANJA", ROSITA: "ROSITA", OSCAR_GAY: "VERDE-CAGUAMA"}
+    color_dictionary = {RED: "ROJO", YELLOW: "AMARILLO", GREEN: "VERDE", BLUE: "AZUL"}
     return color_dictionary.get(color, None)
 
 
 def generate_sound(color):
-    sound_dict = {ROJO: SOUND_1, AZUL: SOUND_2, AMARILLO: SOUND_3, VERDE: SOUND_4, NEGRO: SOUND_1, MORADO: SOUND_2, NARANJA: SOUND_3, ROSITA: SOUND_4, OSCAR_GAY: SOUND_1}
+    sound_dict = {RED: SOUND_1, BLUE: SOUND_2, YELLOW: SOUND_3, GREEN: SOUND_4}
     sound = sound_dict.get(color, None)
     sound.play()
     return sound
 
 
-# muestra la secuencia de colores que el jugador debe de seguir
+# Displays the sequence of colors the player must
 def display_sequence(sequence):
     for color in sequence:
         generate_sound(color)
@@ -133,35 +123,20 @@ def display_sequence(sequence):
 
 # interfaz principal del juego
 def draw_window(color_rectangles, level):
-    screen.fill(BLANCO)
+    screen.fill(WHITE)
 
     # titulo
-    color_game_text = MAIN_FONT.render("MEMORY MATRIX", True, NEGRO)
+    color_game_text = MAIN_FONT.render("MEMORY MATRIX", True, BLACK)
     screen.blit(color_game_text, (WIDTH / 2 - color_game_text.get_width() / 2, 0))
 
     # muestra el texto de nivel
     level_final_text = "Nivel " + level
-    render_level_text = LEVEL_FONT.render(level_final_text, True, NEGRO)
+    render_level_text = LEVEL_FONT.render(level_final_text, True, BLACK)
     screen.blit(render_level_text, (WIDTH / 2 - render_level_text.get_width() / 2, 60))
 
-    # muestra los 9 cuadraditos de colores en una cuadrícula 3x3
-    row_count = 0
-    col_count = 0
-    rect_width = WIDTH // 3
-    rect_height = (HEIGHT - 100) // 3
-
+   # muestra los 4 cuadraditos de colores
     for color, rect in color_rectangles.items():
-        rect.x = col_count * rect_width
-        rect.y = row_count * rect_height + 100
-        rect.width = rect_width
-        rect.height = rect_height
-
         pygame.draw.rect(screen, color, rect)
-
-        col_count += 1
-        if col_count >= 3:
-            col_count = 0
-            row_count += 1
 
     pygame.display.update()
 
@@ -169,9 +144,9 @@ def draw_window(color_rectangles, level):
 # pantalla de juego terminado
 def draw_game_over(text):
     LEVEL_FAILED_SOUND.play()
-    screen.fill(BLANCO)
+    screen.fill(WHITE)
 
-    draw_text = GAME_OVER_FONT.render(text, True, NEGRO)
+    draw_text = GAME_OVER_FONT.render(text, True, BLACK)
     color_text = COLOR_FONT.render(get_color_string(CORRECT_COLOR), True, CORRECT_COLOR)
 
     total_width = draw_text.get_width() + color_text.get_width()
@@ -187,9 +162,9 @@ def draw_game_over(text):
 # dibuja los niveles completados con la ventana
 def draw_level_completed(text):
     LEVEL_PASSED_SOUND.play()
-    screen.fill(BLANCO)
+    screen.fill(WHITE)
 
-    draw_text = LEVEL_COMPLETE_FONT.render(text, True, NEGRO)
+    draw_text = LEVEL_COMPLETE_FONT.render(text, True, BLACK)
 
     text_x = (WIDTH - draw_text.get_width()) // 2
     text_y = (HEIGHT - draw_text.get_height()) // 2
